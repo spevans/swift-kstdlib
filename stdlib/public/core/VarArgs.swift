8b15c4386328c57b035026801e216eee6e401dd2
Simon Evans <si@si.org>
2016-10-30 15:18:55 +0000

    [kernellib] Remove use of FloatingPoint
    - All floating point including Float32, Float64
    
    - Dont compile the following files:
        BuiltinMath.swift.gyb
        FloatingPoint.swift.gyb
        FloatingPointOperations.swift.gyb
        FloatingPointParsing.swift.gyb
    
    - Remove any libc/libcpp functions that use or return float or
      double

--- stdlib/public/core/VarArgs.swift
+++ stdlib/public/core/VarArgs.swift
@@ -290,6 +290,7 @@ extension AutoreleasingUnsafeMutablePointer : CVarArg {
 }
 #endif
 
+#if !KERNELLIB
 extension Float : _CVarArgPassedAsDouble, _CVarArgAligned {
   /// Transform `self` into a series of machine words that can be
   /// appropriately interpreted by C varargs.
@@ -319,6 +320,7 @@ extension Double : _CVarArgPassedAsDouble, _CVarArgAligned {
     return MemoryLayout.alignment(ofValue: self)
   }
 }
+#endif // !KERNELLIB
 
 #if !arch(x86_64)
 
