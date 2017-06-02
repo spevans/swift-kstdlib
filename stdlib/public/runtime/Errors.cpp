spevans/swift

[kernellib] Remove use of libdl functions
- Disable SWIFT_SUPPORTS_BACKTRACE_REPORTING in Errors.cpp
  since it makes use of dladdr(), needs a better solution to
  support backtraces

- Modify swift::_swift_initializeCallbacksToInspectDylib()
  to take a the address of the `__swift2_protocol_conformances_start'
  data and avoid use of dlopen()/dlsym()/dlclose() and
  dl_iterate_phdr()

stdlib/public/runtime/Errors.cpp
@@ -14,7 +14,7 @@
 //
 //===----------------------------------------------------------------------===//
 
-#if defined(__CYGWIN__) || defined(__ANDROID__) || defined(_MSC_VER)
+#if defined(__CYGWIN__) || defined(__ANDROID__) || defined(_MSC_VER) || KERNELLIB
 #  define SWIFT_SUPPORTS_BACKTRACE_REPORTING 0
 #else
 #  define SWIFT_SUPPORTS_BACKTRACE_REPORTING 1

spevans/swift-kstdlib

stdlib/public/runtime/Errors.cpp
@@ -14,7 +14,7 @@
 //
 //===----------------------------------------------------------------------===//
 
-#if defined(__CYGWIN__) || defined(__ANDROID__) || defined(_WIN32)
+#if defined(__CYGWIN__) || defined(__ANDROID__) || defined(_WIN32) || KERNELLIB
 #  define SWIFT_SUPPORTS_BACKTRACE_REPORTING 0
 #else
 #  define SWIFT_SUPPORTS_BACKTRACE_REPORTING 1