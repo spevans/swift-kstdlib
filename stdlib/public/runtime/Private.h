91b59886a8d625179ecbe881dd5843285c192ae7
Simon Evans <si@si.org>
2017-07-30 15:31:51 +0100

    [kernellib] Replace use of std::call_once() with swift_once_f()
    - Using std::call_once() involved use of TLS (.tdata / .tbss) and
      then extra code in the pthread emulation to support __once_proxy().
    
    - Use an external function `swift_once_f()` instead, which allows
      a simpler implementation and no TLS setup.

--- stdlib/public/runtime/Private.h
+++ stdlib/public/runtime/Private.h
@@ -336,4 +336,10 @@ public:
 
 } // end namespace swift
 
+#if KERNELLIB
+extern "C"
+void swift_once_f(uintptr_t *predicate, void (*function)(void *),
+                  void *context);
+#endif
+
 #endif /* SWIFT_RUNTIME_PRIVATE_H */
